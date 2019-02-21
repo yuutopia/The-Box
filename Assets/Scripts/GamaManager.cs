a1c7b3b72d0badc03ccbf8316702ca5d94fa4f62
uehara_yu <yuutopia.sg77@gmail.com>
2019-02-22 03:43:59 +0900

    The Box MASTER

---
+++ Assets/Scripts/GamaManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GamaManager : MonoBehaviour {
+
+    public const int WALL_FRONT = 1;
+    public const int WALL_RIGHT = 2;
+    public const int WALL_BACK = 3;
+    public const int WALL_LEFT = 4;
+
+    public GameObject panelWalls;
+
+    private int wallNo;
+
+    // Use this for initialization
+    void Start () {
+        wallNo = WALL_FRONT;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
